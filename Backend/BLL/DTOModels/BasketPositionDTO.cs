0e62bb3054d7fa5a859e87d867136bcf486a0024
Student RM4-3073 06 <studentlab1406@it.polsl.pl>
2025-03-12 17:21:51 +0100

    ćwiczenie 3

---
+++ Backend/BLL/DTOModels/BasketPositionDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOModels
+{
+    public record BasketPositionRequestDTO
+    {
+        public int ProductId { get; init; }
+        public int Amount { get; init; }
+    }
+    public record BasketPositionResponseDTO
+    {
+        public int ProductId { get; init; }
+        public string ProductName { get; init; }
+        public double ProductPrice { get; init; }
+        public int Amount { get; init; }
+        public double TotalPrice => ProductPrice * Amount;
+
+    }
+}
---
+++ Backend/BLL/Enums/SortOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Enums
+{
+    public enum SortOrder
+    {
+        NameAscending,
+        NameDescending,
+        PriceAscending,
+        PriceDescending,
+        DateAscending,
+        DateDescending
+    }
+}
